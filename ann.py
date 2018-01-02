0c58d9a49ba40e44e12b00b694bb1f7f636a5211
meetkumar <meetkumarpate.1504@gmail.com>
2018-01-02 12:02:40 -0800

    Completed Data Pre processing

--- ann.py
+++ ann.py
@@ -12,9 +12,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
-y = dataset.iloc[:, 4].values
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, [3, 13]].values
+y = dataset.iloc[:, 13].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
@@ -35,3 +35,11 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
